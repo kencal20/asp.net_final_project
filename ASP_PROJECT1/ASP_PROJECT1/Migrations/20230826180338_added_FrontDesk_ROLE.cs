ae7e0109b78dfbc8ea60c7ddd232ddba58873f41
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-28 20:49:15 +0000

    asp.net final project initialization

---
+++ ASP_PROJECT1/ASP_PROJECT1/Migrations/20230826180338_added_FrontDesk_ROLE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_PROJECT1.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_FrontDesk_ROLE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
